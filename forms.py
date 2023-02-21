ebc5469e9508c88c36d9367a0a837a980b4db95c
IDGS-801-20001476 <122839604+IDGS-801-20001476@users.noreply.github.com>
2023-02-20 23:26:23 -0600

    segundo commit 20\02\2023

--- forms.py
+++ forms.py
@@ -2,11 +2,17 @@ from wtforms import Form
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,  FieldList, FormField, SelectField
 from wtforms.fields import EmailField, TextAreaField, RadioField, PasswordField
+from wtforms import validators
 
 
 class UserForm(Form):
-    nombre = StringField('Matricula')
-    nombre = StringField('Nombre')
+    nombre = StringField('Matricula',[
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4,max=10,message='long de campo 5 min and 5 max')
+    ])
+    nombre = StringField('Nombre',[
+        validators.DataRequired(message='El campo es requerido')
+    ])
     apaterno = StringField('Apaterno')
     amaterno = StringField('Amaterno')
     email =  EmailField('Correo')
