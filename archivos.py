2abf42ac1688c2ebe9fddd30bd64a73f06c92f39
IDGS-801-20001476 <122839604+IDGS-801-20001476@users.noreply.github.com>
2023-02-23 22:38:47 -0600

    segundo commit 23\02\2023

--- archivos.py
+++ archivos.py
@@ -1,9 +1,19 @@
 
-file=open('alumnos.txt','r')
-nombres=file.readlines()
+"""
+r= leer
+w=escribir
+a= adiciona contenido al archivo
+
+file=open('alumnos.txt','w')
+nombres=file.readline()
 print(nombres)
-file.close()
+file.close()"""
 """
 for item in nombres:
     print(item,end='')
 """
+
+file=open('alumnos2.txt','a')
+file.write('\n '+' ¡Nuevo Hola mundo !!')
+file.write('\n '+'¡Nuevo Hola mundo 2 !!')
+file.close()
